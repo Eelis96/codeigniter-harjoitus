224a05c249566cb2bbdb2f7a777bf23d7097458e
Eelis96 <eelislantinen14@gmail.com>
2021-02-09 09:02:16 +0200

    first commit

--- application/views/posts/edit.php
+++ application/views/posts/edit.php
@@ -20,5 +20,5 @@
             <?php endforeach; ?>
         </select>
     </div>
-    <button type="submit" class="btn btn-default">Lähetä</button>
+    <button type="submit" class="btn btn-default">Lähetää</button>
 </form>
